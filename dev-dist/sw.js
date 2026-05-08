4af8a8772193f134bf6f0bae499af938546d8137
玉川寛章 <tamagawa@MacBook-Air-2.local>
2026-05-08 22:16:08 +0900

    “update223”

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/RaimuNoteSNS.github.io/index.html",
-    "revision": "0.tohambqoqco"
+    "revision": "0.u6o5q6udkvo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/RaimuNoteSNS.github.io/index.html"), {
